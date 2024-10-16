49915447f3c13be07e561b6d0876e4e454153039
vivekt20 <vivekcherupoika20@gmail.com>
2024-10-16 12:31:40 +0530

    16-10-24

---
+++ android/app/src/main/kotlin/com/example/hive_todo_exam/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.hive_todo_exam
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/Todo.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+class TODO extends StatefulWidget {
+  
+
+  @override
+  State<TODO> createState() => _TODOState();
+}
+
+class _TODOState extends State<TODO> {
+  late Box box;
+  TextEditingController list=TextEditingController();
+  List <String> todoitems=[];
+  @override
+  void initState(){
+    super.initState();
+    box=Hive.box('mybox');
+    _loadtodoit();
+  }
+  _loadtodoit()async{
+List<String>? task=box.get('todoitems');{
+  if(task !=null){
+    setState(() {
+      todoitems=task;
+    });
+  }
+}
+  }
+  _savetodoitem(){
+    setState(() {
+      box.put('todoitem', 'todoitems');
+    });
+  }
+   void _addtodoitem(String item){
+    setState(() {
+      if (item.isNotEmpty){
+        todoitems.add(list.text);
+      list.clear();
+      }
+    });
+    _savetodoitem();
+    
+  }
+void _removetodoitem(int index){
+  setState(() {
+    todoitems.removeAt(index);
+  });
+  _savetodoitem();
+}
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('TODO With HIVE'),
+      backgroundColor: Colors.yellow,),
+      body: Padding(
+        padding: const EdgeInsets.all(15.0),
+        
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+        SizedBox(
+          height: 60,
+          width: 300,
+          child: TextField(
+            controller: list,
+            decoration: InputDecoration(border: OutlineInputBorder()),)),
+        SizedBox(height: 10),
+        ElevatedButton(onPressed: (){
+          _addtodoitem('task');
+        }, child: Text('Add')),
+        Expanded(child: ListView.builder(
+          itemCount: todoitems.length,
+          itemBuilder: (context,index){
+          return ListTile(
+            title: Text(todoitems[index],
+          ),
+          trailing: GestureDetector(
+            onTap: () {
+        setState(() {
+          _removetodoitem(index);
+          
+        },);
+            },child: Icon(Icons.delete,color: Colors.red,)
+          ),
+          );
+        }))
+        
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
